1436ba60b91dbaac7d40aaf2e5c7e3027fe429dc
Patrick-Wilton <u6050506@anu.edu.au>
2019-04-23 13:11:38 +1000

    Stuff

--- Software/Initial Code/netstream/piopencvtest.py
+++ Software/Initial Code/netstream/piopencvtest.py
@@ -10,19 +10,17 @@ import picamera
 
 
 class CameraEvent:
-    """An Event-like class that signals all active clients when a new frame is available."""
+    """The Event class which signals main script when a new camera frame is available"""
     def __init__(self):
         self.events = {}
 
     def wait(self):
-        """Invoked from each client's thread to wait for the next frame."""
         ident = get_ident()
         if ident not in self.events:
             self.events[ident] = [threading.Event(), time.time()]
         return self.events[ident][0].wait()
 
     def set(self):
-        """Invoked by the camera thread when a new frame is available."""
         now = time.time()
         remove = None
         for ident, event in self.events.items():
@@ -36,59 +34,47 @@ class CameraEvent:
             del self.events[remove]
 
     def clear(self):
-        """Invoked from each client's thread after a frame was processed."""
         self.events[get_ident()][0].clear()
 
-
 class StreamOutput():
+    """Defines how the MJPEG stream writes to the buffer and splits each frame"""
     def __init__(self):
         self.frame = None
         self.stream = io.BytesIO()
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
-            self.frame = self.stream.seek(0)
+            self.stream.seek(0)
+            self.frame = self.stream.getvalue()
+            self.stream.truncate()
+            self.stream.seek(0)
+        return self.stream.write(buf)
 
 
 class Camera:
+    """Obtains image, stores camera settings and performs image processing"""
     def __init__(self, settings_cache):
         self.settings = settings_cache
+
+        # Calculates crop and roi sizes
         self.res_height = 720
         self.res_width = 1280
-        self.crop = self.crop_size(600, 600)
+        self.crop = self.crop_size(600, 600, self.res_height, self.res_width)
         self.roi = np.zeros([2, 4])
-        self.roi[0] = self.crop_size(400, 400)
-        self.roi[1] = self.crop_size(250, 250)
-        self.last_access = time.time()
+        self.roi[0] = self.crop_size(400, 400, 600, 600)
+        self.roi[1] = self.crop_size(250, 250, 600, 600)
+        self.roi = self.roi.astype(int)
+
+        # Starts camera thread and initiates event class
         self.thread = threading.Thread(target=self._thread)
         self.thread.start()
-        self.test = io.BytesIO()
-        self.bytes = bytes(self.test)
         self.event = CameraEvent()
 
-    def get_frame(self):
-        self.last_access = time.time()
-        self.event.wait()
-        self.event.clear()
-        return self.frame
-
-    def _thread(self):
-        print('Starting Camera Thread.')
-        frames_iterator = self.frames()
-        for frame in frames_iterator:
-            print(np.shape(list(frame)))
-            self.frame = frame
-            self.event.set()
-            if bool(self.settings):
-                if not self.settings["cam_state"]:
-                    print('Stopping Camera Thread')
-                    break
-
-    def crop_size(self, w, h):
+    def crop_size(self, h, w, rh, rw):
         crop_points = []
-        crop_points.append(int((self.res_height/2) - (h/2)))
+        crop_points.append(int((rh / 2) - (h / 2)))
         crop_points.append(crop_points[0] + h)
-        crop_points.append(int((self.res_width / 2) - (w / 2)))
+        crop_points.append(int((rw / 2) - (w / 2)))
         crop_points.append(crop_points[2] + w)
         return np.array(crop_points)
 
@@ -97,17 +83,31 @@ class Camera:
             self.roi_setting = self.settings["roi"]
             if int(picamera.contrast) != int(self.settings["cam_contrast"]):
                 picamera.contrast = int(self.settings["cam_contrast"])
-            if int(picamera.brightness) != int(self.settings["cam_brightness"]):
-                picamera.brightness = int(self.settings["cam_brightness"])
         except KeyError:
-            print('no entry under that name')
+            print('No setting of that name currently in json file')
+
+    def get_frame(self):
+        self.event.wait()
+        self.event.clear()
+        return self.frame
+
+    def _thread(self):
+        print('Starting Camera Thread')
+        frames_iterator = self.frames()
+        for frame in frames_iterator:
+            self.frame = frame
+            self.event.set()
+            if bool(self.settings):
+                if not self.settings["cam_state"]:
+                    print('Stopping Camera Thread')
+                    break
 
     def frames(self):
-        with picamera.PiCamera(resolution=(self.res_width, self.res_height)) as picamera:
+        with picamera.PiCamera(resolution=(self.res_width, self.res_height)) as self.pi_camera:
             time.sleep(1)
             output = StreamOutput()
-            picamera.start_recording(output, format='mjpeg')
-            picamera.rotation = 180
+            self.pi_camera.start_recording(output, format='mjpeg')
+            self.pi_camera.rotation = 180
             try:
                 while True:
                     try:
@@ -128,7 +128,7 @@ class Camera:
 
                         if self.roi_setting in ("Large", "Small"):
                             roi_img = img[self.roi[roi_index][0]: self.roi[roi_index][1], self.roi[roi_index][2]: self.roi[roi_index][3]]
-                            clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(5, 5))
+                            clahe = cv2.createCLAHE(clipLimit=6.0, tileGridSize=(6, 6))
                             roi_img = clahe.apply(roi_img)
                             img_final[self.roi[roi_index][0]: self.roi[roi_index][1], self.roi[roi_index][2]: self.roi[roi_index][3]] = roi_img
                         yield cv2.imencode('.jpg', img_final)[1].tobytes()
@@ -139,4 +139,4 @@ class Camera:
                     except cv2.error:
                         pass
             finally:
-                picamera.stop_recording()
+                self.pi_camera.stop_recording()
